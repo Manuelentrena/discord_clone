f6abcd254a0d9250ef114b216cce63d87ff894ce
EntrenaDev <manuel.entrena@gmail.com>
2023-09-04 21:51:16 +0200

    Added new modal create server and aciton create server

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import type { Metadata } from "next";
+import { cn } from "@/lib/utils";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -29,6 +30,7 @@ export default function RootLayout({
               enableSystem={false}
               storageKey="discord-theme"
             >
+              <ModalProvider />
               {children}
             </ThemeProvider>
           </body>
