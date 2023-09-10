9cf95d87f28e9b7c75a7dd76e10d7d550fb3c73a
EntrenaDev <manuel.entrena@gmail.com>
2023-09-10 12:38:30 +0200

    Fix bug name default of chat general

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
         inviteCode: uuidv4(),
         channels: {
           create: {
-            name: "General",
+            name: "general",
             profileId: profile.id,
           },
         },
