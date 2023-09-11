4d9ed17f486ccbe18f861714fee349fa550d7745
EntrenaDev <manuel.entrena@gmail.com>
2023-09-11 20:59:22 +0200

    Fix bug toggle theme and navigation sidebar

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -4,37 +4,49 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+import { ActionTooltip } from "@/components/action-tooltip";
+
+const Themes = {
+  LIGHT: "light",
+  DARK: "dark",
+};
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const changeTheme = () => {
+    theme === Themes.LIGHT ? setTheme(Themes.DARK) : setTheme(Themes.LIGHT);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          className="bg-transparent border-0"
-          variant="outline"
-          size="icon"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <ActionTooltip
+      label={`Theme ${theme === Themes.LIGHT ? Themes.DARK : Themes.LIGHT}`}
+      side="right"
+      align="center"
+    >
+      <Button
+        onClick={changeTheme}
+        className=" dark:bg-neutral-700  text-emerald-500 hover:text-white rounded-[24px] h-[48px] w-[48px] mx-3 dark:hover:bg-emerald-500 hover:bg-emerald-500 hover:rounded-[16px] transition-all"
+        variant="outline"
+        size="icon"
+      >
+        {theme === "dark" ? (
+          <Sun className="h-[1.2rem] w-[1.2rem]" />
+        ) : (
+          <Moon className="h-[1.2rem] w-[1.2rem]" />
+        )}
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </ActionTooltip>
   );
 }
