7b3c1c6d6ddbad45f0379dfdcfc9b711032c1748
EntrenaDev <manuel.entrena@gmail.com>
2023-09-12 20:03:34 +0200

    Fix bugs in useAvatar and server header

--- hooks/use-avatar.tsx
+++ hooks/use-avatar.tsx
@@ -9,7 +9,10 @@ interface UserAvatarProps {
 export const UserAvatar = ({ src, className }: UserAvatarProps) => {
   return (
     <Avatar className={cn("h-7 w-7 md:h-10 md:w-10", className)}>
-      <AvatarImage src={src} />
+      <AvatarImage
+        src={src}
+        className={cn("h-7 w-7 md:h-10 md:w-10", className)}
+      />
     </Avatar>
   );
 };
