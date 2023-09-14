6f36be1d21fb8c098585753a27bbf98608ab6d9c
EntrenaDev <manuel.entrena@gmail.com>
2023-09-14 22:12:52 +0200

    Added socket io library, implements connection in provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { Open_Sans } from "next/font/google";
@@ -30,8 +31,10 @@ export default function RootLayout({
               enableSystem={false}
               storageKey="discord-theme"
             >
-              <ModalProvider />
-              {children}
+              <SocketProvider>
+                <ModalProvider />
+                {children}
+              </SocketProvider>
             </ThemeProvider>
           </body>
         </html>
