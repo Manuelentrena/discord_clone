e175a7458dcfb9d955a7cee49a90a4c467f60756
EntrenaDev <manuel.entrena@gmail.com>
2023-09-18 21:02:30 +0200

    Added react-query, create useChatQuery

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import type { Metadata } from "next";
 import { cn } from "@/lib/utils";
 import { Open_Sans } from "next/font/google";
@@ -33,7 +34,7 @@ export default function RootLayout({
             >
               <SocketProvider>
                 <ModalProvider />
-                {children}
+                <QueryProvider>{children}</QueryProvider>
               </SocketProvider>
             </ThemeProvider>
           </body>
