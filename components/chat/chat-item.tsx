4a205405dc8edd2f301332c7be9fede1d4d14bc2
EntrenaDev <manuel.entrena@gmail.com>
2023-09-21 18:44:35 +0200

    Show direct messages on click avatar profile

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Member, MemberRole, Profile } from "@prisma/client";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 
 import { UserAvatar } from "@/hooks/use-avatar";
@@ -56,6 +56,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const { onOpen } = useModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return;
+    }
+
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: any) => {
@@ -114,7 +124,7 @@ export const ChatItem = ({
     <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
       <div className="group flex gap-x-2 items-start w-full">
         <div
-          // onClick={onMemberClick}
+          onClick={onMemberClick}
           className="cursor-pointer hover:drop-shadow-md transition"
         >
           <UserAvatar src={member.profile.imageUrl} />
@@ -123,7 +133,7 @@ export const ChatItem = ({
           <div className="flex items-center gap-x-2">
             <div className="flex items-center">
               <p
-                // onClick={onMemberClick}
+                onClick={onMemberClick}
                 className="font-semibold text-sm hover:underline cursor-pointer"
               >
                 {member.profile.name}
